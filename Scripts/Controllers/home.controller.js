93d270a37730d2c0fffc8b8a11b7429c5d69b7df
Thineskumar Thilakanathan <skwebbies@hotmail.co.uk>
2016-12-11 01:03:14 +0100

    Initial

---
+++ Scripts/Controllers/home.controller.js
@@ -0,0 +1,28 @@
+﻿/// <reference path="../angular.js" />
+
+angular.module("mainModule")
+    .controller("HomeController",
+    [
+        "$scope",
+        "weatherService",
+        function($scope, weatherService) {
+            $scope.title = "Weather Reports";
+
+            $scope.weather = [];
+
+
+
+            weatherService.GetAllWeatherReport()
+                .then(function (data) {
+                    if (data != null) {
+                    $scope.weather = data;
+                    console.log($scope.weather);
+                    }
+
+            });
+
+
+
+
+        }
+    ]);
---
+++ Scripts/Routes/route.js
@@ -0,0 +1,26 @@
+﻿/// <reference path="../angular.js" />
+
+angular.module("mainModule")
+    .config([
+        "$routeProvider",
+        "$locationProvider",
+
+        function($routeProvider, $locationProvider) {
+            $locationProvider.html5Mode(true);
+
+            $routeProvider
+                .when("/",
+                {
+                    templateUrl: "Views/Home.html",
+                    controller: "HomeController",
+                    caseInsensitiveMatch: true,
+                    activeTab: 'Home'
+
+                })
+                .otherwise({
+                    redirectTo: ("/")
+                });
+
+
+        }
+    ]);
